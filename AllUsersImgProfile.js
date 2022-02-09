bb7c2193c7064f1d4db5539a40fa7b78706e619a
LAPCoder <97749743+LAPCoder@users.noreply.github.com>
2022-02-09 18:51:12 +0100

    Create AllUsersImgProfile.js

---
+++ AllUsersImgProfile.js
@@ -0,0 +1,6 @@
+document.write("<DOCTYPE html>\n<html><body><h1>GitHub users:</h1>");
+for(i=1; i<993/*50000*/; i++)
+{
+	document.write("<img src=\"https://avatars.githubusercontent.com/u/", i, "\" style=\"height: 80px;\"></a>");
+}
+document.write("</body></html>")
